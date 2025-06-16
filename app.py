d679f20dd5966a85efbd9a4f103fdd9c2950f9b6
Vinicius Btechs <viniciuscortat@hotmail.com>
2025-06-16 20:19:02 -0300

    Correção de typo

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 car_data = pd.read_csv('vehicles.csv')
 st.title("Gráficos de Veículos")
-st.header("Historgramas")
+st.header("Histogramas")
 hist_button = st.checkbox('Criar histograma')
 valid_cols = car_data.select_dtypes(include=['int64','float64']).columns
 columns_list = valid_cols.tolist()
